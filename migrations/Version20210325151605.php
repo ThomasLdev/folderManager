28ddc73d751a320b2648e1d9d1d61012adc832a3
Thomas LEFEBVRE <tlefebvre@group-dis.com>
2021-09-20 15:43:20 +0200

    fix migrations issues on FK

--- migrations/Version20210325151605.php
+++ migrations/Version20210325151605.php
@@ -20,12 +20,12 @@ final class Version20210325151605 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE folder_option (folder_id INT NOT NULL, option_id INT NOT NULL, INDEX IDX_BF3E7AC6162CB942 (folder_id), INDEX IDX_BF3E7AC6A7C41D6F (option_id), PRIMARY KEY(folder_id, option_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE folder_option ADD CONSTRAINT FK_BF3E7AC6162CB942 FOREIGN KEY (folder_id) REFERENCES folder (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE folder_option ADD CONSTRAINT FK_BF3E7AC6A7C41D6F FOREIGN KEY (option_id) REFERENCES `option` (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE `option` DROP FOREIGN KEY FK_5A8600B0162CB942');
-        $this->addSql('DROP INDEX IDX_5A8600B0162CB942 ON `option`');
-        $this->addSql('ALTER TABLE `option` DROP folder_id');
+//        $this->addSql('CREATE TABLE folder_option (folder_id INT NOT NULL, option_id INT NOT NULL, INDEX IDX_BF3E7AC6162CB942 (folder_id), INDEX IDX_BF3E7AC6A7C41D6F (option_id), PRIMARY KEY(folder_id, option_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+//        $this->addSql('ALTER TABLE folder_option ADD CONSTRAINT FK_BF3E7AC6162CB942 FOREIGN KEY (folder_id) REFERENCES folder (id) ON DELETE CASCADE');
+//        $this->addSql('ALTER TABLE folder_option ADD CONSTRAINT FK_BF3E7AC6A7C41D6F FOREIGN KEY (option_id) REFERENCES `option` (id) ON DELETE CASCADE');
+//        $this->addSql('ALTER TABLE `option` DROP FOREIGN KEY FK_5A8600B0162CB942');
+//        $this->addSql('DROP INDEX IDX_5A8600B0162CB942 ON `option`');
+//        $this->addSql('ALTER TABLE `option` DROP folder_id');
     }
 
     public function down(Schema $schema) : void
